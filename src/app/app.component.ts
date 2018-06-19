c2d21160baf12fe9029cd40a50508117bc73a1c1
a.chebotareva <sabermeyt@gmail.com>
2018-06-19 20:58:58 +0300

    stage 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../../users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [UsersService]
 })
 export class AppComponent {
-  title = 'app';
+  users = [];
+  constructor (private userService: UsersService) {
+  }
+  ngOnInit() {
+    // this.users = this.userService.users;
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
 }
---
+++ users.service.ts
@@ -0,0 +1,28 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class UsersService {
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getUsers() {
+    return this.httpClient.get('https://randomuser.me/api/?inc=gender,name,picture,location&results=8&nat=gb')
+      .map(res => res.results)
+      .map(users => {
+        return users.map(u => {
+          return {
+            name: u.name.first + ' ' + u.name.last,
+            image: u.picture.large,
+            geo: u.location.city + ', ' + u.location.state + ', ' + u.location.street
+          };
+          }
+        );
+        }
+      );
+
+  }
+
+}
