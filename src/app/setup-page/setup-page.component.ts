bc05b01089ca3744da80eaa511c75b8cde5402a9
a.chebotareva <sabermeyt@gmail.com>
2018-07-26 21:34:15 +0300

    real fin.

---
+++ src/app/setup-page/setup-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../users.service';
+
+@Component({
+  selector: 'app-setup-page',
+  templateUrl: './setup-page.component.html',
+  styleUrls: ['./setup-page.component.scss']
+})
+export class SetupPageComponent implements OnInit {
+
+  size;
+  constructor(private userService: UsersService) { }
+
+  ngOnInit() {
+  }
+  onChange() {
+    this.userService.setSize(+this.size);
+  }
+
+}
